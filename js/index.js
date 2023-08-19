eb0c7f0f7664516bd8009676405a6dd281c094f3
mohamed <midoelharery@gmail.com>
2023-08-19 16:59:00 +0300

    GitHub Repos

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ btn.onclick = () => {
         // parentRepos.style.background = "#eee";
         for (let i = 0; i < result.length; i++) {
           userNameRepo.id = "userName-repo";
-          userNameRepo.innerHTML = `Repos: ${user.value}`;
+          userNameRepo.innerHTML = `Repositories : ${user.value}`;
           parentRepos.className = "repos";
           let createReopDiv = document.createElement("div");
           createReopDiv.className = "repo";
@@ -37,14 +37,11 @@ btn.onclick = () => {
           let repoName = document.createTextNode(result[i].name);
           repoNameA.href = `https://${user.value}.github.io/${result[i].name}`;
           repoNameA.target = "blank"
-          // repoNameA.href = `https://${user.value}.github.io/${result[i].name}`;
-          // repoNameA.append(document.createTextNode(result[i].name));  
-          //
           createReopDiv.appendChild(repoNameA);
           parentRepos.before(userNameRepo);
           repoNameA.appendChild(repoName);
           parentRepos.appendChild(createReopDiv);
-          document.body.appendChild(parentRepos);
+          document.body.append(parentRepos);
           // repos.appendChild(createReopDiv)
         }
         user.value = "";
